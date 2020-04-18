fe867934e7dc8cbedb976ad7ae6f168cd92f4f04
vrnranjan22 <vrnranjan22@gmail.com>
2020-04-18 17:36:59 +0530

    minor update

--- oops/class_obj_refVar.py
+++ oops/class_obj_refVar.py
@@ -1,7 +1,11 @@
 class Student:
-    '''
-    This is a class with with method as __init__ and talk 
-    '''
+    """
+     Here whatever is under the indentation of class is know as class
+     class have methods,
+     On the basis of class we make object.In line 14 we have make an object
+     name as "Student(123,'rahul)" and the variable which is referencing this
+     object is known as reference variable. Here "s" is a reference variable
+    """
     def __init__(self, rollNo, name):
         self.roll_Number = rollNo
         self.name = name
@@ -13,11 +17,5 @@ class Student:
 
 s=Student(123,'rahul')
 s.talk()
+help(Student)
 
-"""
- Here whatever is under the indentation of class is know as class
- class have methods,
- On the basis of class we make object.In line 14 we have make an object 
- name as "Student(123,'rahul)" and the variable which is referencing this 
- object is known as reference variable. Here "s" is a reference variable
-"""
