607b24796f54f843066fcfad1b04e1bf9332d4b3
vrnranjan22 <vrnranjan22@gmail.com>
2020-04-18 10:49:21 +0530

    oops

---
+++ oops/class_and_instance.py
@@ -0,0 +1,17 @@
+# class employee:
+#     def __init__(self, first, last):
+#         self.first = first
+#         self.last = last
+#         self.name = first + " "+ last
+#
+#     def full_name(self):
+#         return (f"Hello, {self.first}. You are {self.last}.")
+#
+#
+# emp1 = employee('varun', 'ranjan' )
+# print(emp1.full_name())
+# # print( f"Hello, {emp1.first}. You are {emp1.last}.")
+
+import os
+print(os.listdir())
+
